904ad33557204a1d77b7499af20b278e8e550e75
a.dacapo21 <aggeloskappos@gmail.com>
2024-06-19 12:29:53 +0200

    Nodejs-Boilerplate/ - add format & lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
-import { ESLint } from 'eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
 export default [
     {
         files: ['src/**/*.{js,ts}'],
         ignores: ['node_modules/', 'dist/'],
         languageOptions: {
-            parser: '@typescript-eslint/parser',
+            parser,
             parserOptions: {
                 project: './tsconfig.json',
             },
         },
         plugins: {
-            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-            prettier: require('eslint-plugin-prettier'),
+            '@typescript-eslint': eslintPluginTypescript,
+            prettier: eslintPluginPrettier,
         },
         rules: {
             'prettier/prettier': 'error',
--- src/commons/database/connection.ts
+++ src/commons/database/connection.ts
@@ -4,18 +4,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export class DatabaseConnection {
-    private static pool: Pool;
+  private static pool: Pool;
 
-    static getInstance(): Pool {
-        if (!DatabaseConnection.pool) {
-            DatabaseConnection.pool = new Pool({
-                user: process.env.DB_USER,
-                host: process.env.DB_HOST,
-                database: process.env.DB_NAME,
-                password: process.env.DB_PASSWORD,
-                port: Number(process.env.DB_PORT),
-            });
-        }
-        return DatabaseConnection.pool;
+  static getInstance(): Pool {
+    if (!DatabaseConnection.pool) {
+      DatabaseConnection.pool = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+      });
     }
+    return DatabaseConnection.pool;
+  }
 }
