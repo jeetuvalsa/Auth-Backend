ff2b3aaa47ecae6b3bfa6ee91840d1b7706f1ca7
a.dacapo21 <aggeloskappos@gmail.com>
2024-06-19 17:36:23 +0200

    Nodejs-Boilerplate

---
+++ src/models/user.ts
@@ -0,0 +1,48 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+
+interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: number;
+  public name!: string;
+  public email!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+export const UserFactory = (sequelize: Sequelize) => {
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'users',
+    },
+  );
+
+  return User;
+};
