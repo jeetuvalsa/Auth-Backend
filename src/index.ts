754bc8439f16b7869574c04128be1037c6aea864
jeetuvalsa <valsajeetu135@gmail.com>
2025-02-22 22:20:44 +0530

    Update CORS configuration to allow all origins

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  })
-);
+app.use(cors());
 
 app.use("/api/users", userRoute);
 
